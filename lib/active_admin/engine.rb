75b1262fbf08471dba9be9507aed9d7b4bb01584
Yoav <yoav@gingerlime.com>
2014-11-20 13:26:41 +0000

    trying fix for #3637

--- lib/active_admin/engine.rb
+++ lib/active_admin/engine.rb
@@ -1,7 +1,7 @@
 module ActiveAdmin
   class Engine < ::Rails::Engine
     initializer "active_admin.precompile", group: :all do |app|
-      app.config.assets.precompile += %w(active_admin.js active_admin.css active_admin/print.css)
+      # app.config.assets.precompile += %w(active_admin.js active_admin.css active_admin/print.css)
     end
   end
 end
